5e414045077d096bc90928703ba64560eb6b2ec0
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2021-01-19 13:46:07 +0100

    resena naloga

--- Izpitne_naloge/nekaj_nalog.py
+++ Izpitne_naloge/nekaj_nalog.py
@@ -48,7 +48,7 @@ rad_imam_zaporedja(5, 3)
 
 def mama_franca(dolzina, stevilo, sirina):
 
-    @lru_cache
+    @lru_cache(maxsize=None)
     def slepa_mama(n, m, l):
         stevilo_dobrih_postavitev = 0
         if n == l and m == 1:
@@ -67,3 +67,42 @@ def mama_franca(dolzina, stevilo, sirina):
     return slepa_mama(dolzina, stevilo, sirina)
 
 mama_franca(9, 3, 2)
+
+#Funkcija simetricen, ki preveri ali je niz simetricen ali ne.(Verjetno obstaja se lazje/ hitreje)
+def simetricen(n):
+    niz = n
+    obrnjen = ""
+    if len(n) == 0:
+        return False
+    elif len(n) == 1:
+        return True
+    else:
+        while len(n) > 0:
+            z = n[-1]
+            obrnjen += z
+            n = n[:-1]
+        if niz == obrnjen:
+            return True
+        else:
+            return False
+
+# Stevilo delov bomo pogledali na naslednji nacin:
+# Na zacetku preverimo, ali je nas niz ze palindrom, in ce je bo funkcija vrnila 1, saj pri ponovne klicu klicemo s praznim nizom (niz[:0] == "")
+# Ce pa ta ni palindrom, se bomo nazaj klicali z ena vecjim indeksom, in to ponavljali, dokler ne najdemo palindroma. Potem pa se klicemo z nizom brez ze najdenega palindroma in indexom 0.
+# Primer (ABC, 0) -> (ABC , 1) -> (ABC, 2) , C je palindrom -> 1 + (AB, 0) -> 1 + (AB, 1), B je palindrom -> 1 + 1 + (A, 0) -> A je palindrom -> 3 
+
+def stevilo_delov(niz):
+
+    @lru_cache(maxsize=None)
+    def prestej_pomozna(niz ,i):
+        dobri_deli = 0
+        if len(niz) == 0:
+            return dobri_deli
+        elif simetricen(niz[i:]) == False:
+            return dobri_deli + prestej_pomozna(niz, i + 1)
+        elif simetricen(niz[i:]) == True:
+            dobri_deli += 1
+            return dobri_deli + prestej_pomozna(niz[:i], 0)
+        return dobri_deli
+
+    return prestej_pomozna(niz, 0)
